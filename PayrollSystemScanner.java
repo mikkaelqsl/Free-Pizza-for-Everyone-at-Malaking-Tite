b31a2f33cb04ee6f4c8c4cf5a75ef68354ecd0f8
mikkaelqsl <mikkael.qsl@gmail.com>
2026-08-09 12:56:34 +0800

    Update PayrollSystemScanner.java

--- PayrollSystemScanner.java
+++ PayrollSystemScanner.java
@@ -8,6 +8,7 @@ public class PayrollSystemScanner{
         double basic_salary, overtime_hours, rate_per_hour, rice_allowance, transportation_allowance, tax_deduction;
         
         //get inputs
+        //String inputs use nextLine() so spaces in names and department names can be accepted
         System.out.print("Enter name                        :   ");
         name = input.nextLine();
         System.out.print("Enter Employee ID                 :   ");
@@ -28,19 +29,42 @@ public class PayrollSystemScanner{
         tax_deduction = input.nextDouble();
         
         //process inputs
+        //all calculated salary values are stored separately so they can be displayed individually in the receipt
         double sss, philhealth, pagibig, tax, total_deduction, gross_pay, overtime_pay, net_pay;
+        
+        //monthly salary is converted to an estimated hourly salary before calculating overtime pay
         overtime_pay = ((((basic_salary * 12) / 52) / 40) * rate_per_hour) * overtime_hours;
+        
+        //gross pay consists of the basic salary, overtime pay, and both allowances
         gross_pay = basic_salary + overtime_pay + rice_allowance + transportation_allowance;
+        
+        //mandatory deductions are calculated as percentages of the gross pay
         sss = gross_pay * 0.05;
         philhealth = gross_pay * 0.025;
         pagibig = gross_pay * 0.02;
+        
+        //tax_deduction is entered as a whole-number percentage, so it is divided by 100 before multiplication
         tax = (tax_deduction) / 100 * gross_pay;
+        
+        //all individual deductions are combined before calculating the final net salary
         total_deduction = sss + philhealth + pagibig + tax;
+        
+        //net pay is the amount remaining after all deductions are removed from gross pay
         net_pay = gross_pay - total_deduction;
+        
+        //boolean tests are used to check whether the payroll result satisfies the given requirements
         boolean test1, test2, test3, test4;
+        
+        //checks whether the employee's net salary reaches the minimum of P10,000
         test1 = net_pay >= 10000;
+        
+        //gross salary should normally be equal to or greater than net salary because deductions are subtracted
         test2 = gross_pay >= net_pay;
+        
+        //checks whether the employee actually received overtime pay
         test3 = overtime_pay > 0;        
+        
+        //all three conditions must be true for the final requirement to be met
         test4 = test1 && test2 && test3;
         
         //display output
@@ -76,9 +100,13 @@ public class PayrollSystemScanner{
         System.out.println("Net Salary                      :   " + net_pay);
         System.out.println("================================================");
         System.out.println();
+        
+        //display the result of each individual requirement check
         System.out.println("Net salary is P20,000 or above          :   " + test1);
         System.out.println("Gross Salary is greater than Net Salary :   " + test2);
         System.out.println("Employee received overtime pay          :   " + test3);
+        
+        //test4 becomes true only when test1, test2, and test3 are all true
         System.out.println("Both requirements are met               :   " + test4);
     }
 }
